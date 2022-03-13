16a2201c0cd03496f6e0e6a953e8569f73287538
Kjasuquo <joseph.asuquo@decagon.dev>
2022-03-13 16:50:27 +0100

    one server and several clients implemented

--- Client/main.go
+++ Client/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func logFetal(err error) {
@@ -22,15 +25,53 @@ func main() {
 	//close at the end
 	defer connection.Close()
 
-	//read message from the stdin (terminal)
+	fmt.Println("Enter Username")
+
+	//read message/username from the stdin (terminal)
 	reader := bufio.NewReader(os.Stdin)
 	//read every line as a string
-	message, err := reader.ReadString('\n')
+	username, err := reader.ReadString('\n')
 	logFetal(err)
 
-	//Msg := fmt.Sprintf(message)
+	username = strings.Trim(username, "\n")
+	welcomeMessage := fmt.Sprintf("Welcome %s", username)
+	fmt.Println(welcomeMessage)
 
-	//Dial the written message for a server in the same port (:8080) to listen
-	connection.Write([]byte(message))
+	go read(connection)
 
+	write(connection, username)
+
+}
+
+func read(connection net.Conn) {
+	for {
+		reader := bufio.NewReader(connection)
+		message, err := reader.ReadString('\n')
+		if err == io.EOF {
+			connection.Close()
+			fmt.Println("Connection closed")
+			os.Exit(0)
+		}
+		fmt.Println(message)
+		//fmt.Println("-----------------------------------")
+	}
+}
+
+func write(connection net.Conn, username string) {
+	for {
+		//read message from the stdin (terminal)
+		reader := bufio.NewReader(os.Stdin)
+		//read every line as a string
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		//to print out the username and the message sent, use sprintf to assign it to a variable
+		mes := fmt.Sprintf("%s:- %s\n", username, strings.Trim(message, "\n"))
+
+		//Dial the written message for a server in the same port (:8080) to listen
+		connection.Write([]byte(mes))
+
+	}
 }
--- Server/main.go
+++ Server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"net"
 )
@@ -36,21 +35,49 @@ func main() {
 			logFetal(err)
 
 			//If there is connection, and it accepts it, make it true
-			//OpenConnection[conn] = true
+			OpenConnection[conn] = true
 
 			//pass the connection through this channel
 			NewConnection <- conn
 		}
 	}()
 
-	//Save the Channel with the message as a variable for easy usage. or you can use it directly
-	connection := <-NewConnection
-	//Read whatever message that is in the channel that it has received from the client
-	reader := bufio.NewReader(connection)
-	//Read each line as a string
-	message, err := reader.ReadString('\n')
-	logFetal(err)
-	//Print out the message
-	fmt.Println(message)
+	for {
+		select {
+		// if there is something in the channel, invoke broadcast message (broadcast to other connections)
+		case conn := <-NewConnection:
+			go broadcastMessage(conn)
 
+		//Delete the connection if nothing
+		case conn := <-DeadConnection:
+			for item := range OpenConnection {
+				if item == conn {
+					break
+				}
+			}
+			delete(OpenConnection, conn)
+		}
+
+	}
+
+}
+
+func broadcastMessage(conn net.Conn) {
+	for {
+		reader := bufio.NewReader(conn)
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+
+		//loop through all the open connections and send messages to these connections
+		//except the connections that sent the message
+		for item := range OpenConnection {
+			if item != conn {
+				item.Write([]byte(message))
+			}
+		}
+	}
+
+	DeadConnection <- conn
 }
